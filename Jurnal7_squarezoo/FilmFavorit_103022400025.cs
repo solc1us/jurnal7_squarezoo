ec6e1e17ee73c356d8ff807bd4c44149545ec14d
solc1us <jeremimarcelino.jm@gmail.com>
2026-04-18 08:55:46 +0700

    menambahkan class watchlist_103022400025

--- Jurnal7_squarezoo/FilmFavorit_103022400025.cs
+++ Jurnal7_squarezoo/FilmFavorit_103022400025.cs
@@ -7,6 +7,7 @@ namespace Jurnal7_squarezoo
 {
     internal class FilmFavorit_103022400025
     {
+        // Inisialisasi atribut
         public string title { get; set; } = "";
         public string director { get; set; } = "";
         public string year { get; set; } = "";
@@ -17,23 +18,27 @@ namespace Jurnal7_squarezoo
 
         public void readJSON()
         {
+            // Ini path buat ambil file json
             string path = Path.Combine(AppContext.BaseDirectory, "jurnal7_1_103022400025.json");
 
             string json = File.ReadAllText(path);
 
+            // Deserialize
             FilmFavorit_103022400025? filmFavorit_103022400025 = JsonSerializer.Deserialize<FilmFavorit_103022400025>(json);
 
+            // Cek dulu ada apa ngga
             if (filmFavorit_103022400025 is null)
             {
                 Console.WriteLine("Data film tidak dapat dibaca");
             }
 
+            // Print
             Console.WriteLine($"Film {filmFavorit_103022400025.title} - {filmFavorit_103022400025.director}");
-            Console.WriteLine($"\t-{filmFavorit_103022400025.year}");
-            Console.WriteLine($"\t-{filmFavorit_103022400025.genre}");
-            Console.WriteLine($"\t-{filmFavorit_103022400025.rating}");
-            Console.WriteLine($"\t-{filmFavorit_103022400025.durationMinutes}");
-            Console.WriteLine($"\t-{filmFavorit_103022400025.isWatched}");
+            Console.WriteLine($" - Year: {filmFavorit_103022400025.year}");
+            Console.WriteLine($" - Genre: {filmFavorit_103022400025.genre}");
+            Console.WriteLine($" - Rating: {filmFavorit_103022400025.rating}");
+            Console.WriteLine($" - Duration: {filmFavorit_103022400025.durationMinutes}");
+            Console.WriteLine($" - Is Watched: {filmFavorit_103022400025.isWatched}");
         }
     }
 }
--- Jurnal7_squarezoo/Program.cs
+++ Jurnal7_squarezoo/Program.cs
@@ -1,5 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Jurnal7_squarezoo;
 
+// Buat objek dari class FilmFavorit_103022400025
 FilmFavorit_103022400025 filmFavorit_103022400025 = new();
+
+// Panggil method readJSON
 filmFavorit_103022400025.readJSON();
+
+// Buat objek dari class Watchlist_103022400025
+Watchlist_103022400025 watchlist_103022400025 = new();
+
+// Panggil method readJSON
+watchlist_103022400025.readJSON();
