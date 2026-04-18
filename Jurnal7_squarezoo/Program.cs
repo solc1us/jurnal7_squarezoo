1eeeba344ae1409216aeb530ec07f27e16fdb2be
Rayhan <rayhanafdhani2002@gmail.com>
2026-04-18 09:15:01 +0700

    menambahkan cllass genredictionary

--- Jurnal7_squarezoo/Program.cs
+++ Jurnal7_squarezoo/Program.cs
@@ -1,14 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Jurnal7_squarezoo;
 
-// Buat objek dari class FilmFavorit_103022400025
+// Buat objek dari class FilmFavorit_103022400070
 FilmFavorit_103022400070 filmFavorit_103022400070 = new();
 
 // Panggil method readJSON
 filmFavorit_103022400070.readJSON();
 
-// Buat objek dari class Watchlist_103022400025
+// Buat objek dari class Watchlist_103022400070
 Watchlist_103022400070 watchlist_103022400070 = new();
 
 // Panggil method readJSON
 watchlist_103022400070.readJSON();
+
+// Buat objek dari class GenreDictionary_103022400070
+GenreDictionary_103022400070 genreDictionary_103022400070 = new();
+
+// Panggil method readJSON
+genreDictionary_103022400070.ReadJSON();
