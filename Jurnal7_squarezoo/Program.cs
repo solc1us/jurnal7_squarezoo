2ffe238bce025a2fbf5121b2664b30c283110d11
Rayhan <rayhanafdhani2002@gmail.com>
2026-04-18 09:05:12 +0700

    menambahkan class watchlist_103022400070

--- Jurnal7_squarezoo/Program.cs
+++ Jurnal7_squarezoo/Program.cs
@@ -1,5 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Jurnal7_squarezoo;
 
+// Buat objek dari class FilmFavorit_103022400025
 FilmFavorit_103022400070 filmFavorit_103022400070 = new();
+
+// Panggil method readJSON
 filmFavorit_103022400070.readJSON();
+
+// Buat objek dari class Watchlist_103022400025
+Watchlist_103022400070 watchlist_103022400070 = new();
+
+// Panggil method readJSON
+watchlist_103022400070.readJSON();
